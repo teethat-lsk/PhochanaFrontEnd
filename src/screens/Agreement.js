5ec33f182aeab35c6fa3e8d4e9aa5a2b85e1353b
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2021-03-18 14:22:48 +0700

    แก้ UI หน้า home

--- src/screens/Agreement.js
+++ src/screens/Agreement.js
@@ -1,25 +1,34 @@
-import React from "react";
-import "../styles/Agreement.css";
-import ScrollText from "../components/ScrollText";
-import ButtonLink from "../components/ButtonLink";
-import agreementText from "../config/agreement";
+import React from 'react';
+import '../styles/Agreement.css';
+import ScrollText from '../components/ScrollText';
+import ButtonLink from '../components/ButtonLink';
+import agreementText from '../config/agreement';
 
 function Agreement() {
-  return (
-    <div className="agreement">
-      <ScrollText h="calc(78% - 40px)" w="100%">
-        {agreementText}
-      </ScrollText>
-      <div className="agreement_button_zone">
-        <ButtonLink to="/register" height="40px">
-          accept
-        </ButtonLink>
-        <ButtonLink to="/" height="40px" backgroundColor="transparent">
-          never
-        </ButtonLink>
-      </div>
-    </div>
-  );
+	return (
+		<div className='agreement'>
+			<ScrollText
+				h='calc(78% - 30px)'
+				w='100%'
+				className='agreement_text'
+			>
+				{agreementText}
+			</ScrollText>
+			<div className='agreement_button_zone'>
+				<ButtonLink to='/register' height='70px'>
+					ยอมรับ
+				</ButtonLink>
+				<ButtonLink
+					to='/'
+					height='70px'
+					backgroundColor='transparent'
+					fontSize='18px'
+				>
+					ปฏิเสธ
+				</ButtonLink>
+			</div>
+		</div>
+	);
 }
 
 export default Agreement;
