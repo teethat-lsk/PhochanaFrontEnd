e692448cab9c39c2ce6c8f97507c8c39d23c4261
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2021-03-18 13:47:42 +0700

    edit css buttonlink

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ButtonLink from "../components/ButtonLink";
-import Logo from "../components/Logo";
-import "../styles/home.css";
+import React from 'react';
+import ButtonLink from '../components/ButtonLink';
+import Logo from '../components/Logo';
+import '../styles/home.css';
 
 function Home() {
-  return (
-    <div className="home">
-      <div className="logo_layout">
-        <Logo h="200px" w="200px" />
-      </div>
-      <div className="home_button_zone">
-        <ButtonLink to="/login" height="60px">
-          Login
-        </ButtonLink>
-        <ButtonLink to="/agreement" height="60px">
-          Register
-        </ButtonLink>
-      </div>
-    </div>
-  );
+	return (
+		<div className='home'>
+			<div className='logo_layout'>
+				<Logo h='200px' w='200px' />
+			</div>
+			<div className='home_button_zone'>
+				<ButtonLink to='/login' height='70px'>
+					Login
+				</ButtonLink>
+				<ButtonLink to='/agreement' height='70px'>
+					Register
+				</ButtonLink>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
