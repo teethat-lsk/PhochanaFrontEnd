038caa7c0bc17e3dedf9a6887680c411b7ef2e8b
soon-404 <tfpsoon@gmail.com>
2021-03-23 00:40:52 +0700

    fix_soon

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,137 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import Calendar from 'react-calendar';
-import moment from 'moment';
-import validator from 'validator';
-import './Input.css';
+import React, { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import validator from "validator";
+import "./Input.css";
 
 function Input({
-	children,
-	ext,
-	placeholder,
-	type,
-	options,
-	value,
-	onChange,
-	id,
-	minLength = 1,
-	maxLength = 30,
-	max = 100000000000,
-	min = -100000000000,
-	allow = 'paint', // paint, email, username, number
+  children,
+  ext,
+  placeholder,
+  type,
+  options,
+  value,
+  onChange,
+  id,
+  minLength = 1,
+  maxLength = 30,
+  max = 100000000000,
+  min = -100000000000,
+  allow = "plain", // plain, email, username, number
 }) {
-	const options_ = options || [];
-	const [openDatePicker, toggleDatePicker] = useState(false);
-	const [error, setError] = useState();
+  const options_ = options || [];
+  const [openDatePicker, toggleDatePicker] = useState(false);
+  const [error, setError] = useState();
 
-	const onChangeHandleCalendar = (e) => {
-		onChange(moment(e).format('YYYY-MM-DD'));
-		toggleDatePicker(false);
-	};
+  const onChangeHandleCalendar = (e) => {
+    onChange(moment(e).format("YYYY-MM-DD"));
+    toggleDatePicker(false);
+  };
 
-	// Validator
-	const handleValidation = (e) => {
-		let valid = true;
-		let value_ = e.target.value;
+  // Validator
+  const handleValidation = (e) => {
+    let valid = true;
+    let value_ = e.target.value;
 
-		if (value_.length < minLength) {
-			valid = false;
-			setError(`ความยาวต้องมากกว่า ${minLength} ตัวอักษร`);
-		} else if (value_.length > maxLength) {
-			setError(`ความยาวห้ามเกิน ${maxLength} ตัวอักษร`);
-			valid = false;
-		} else if (allow === 'email') {
-			// console.log(validator.isEmail(e.target.value), e.target.value);
-			if (!validator.isEmail(value_)) {
-				setError(`รูปแบบของ email ไม่ถูกต้อง`);
-				valid = false;
-			} else {
-				valid = true;
-			}
-		} else if (allow === 'username') {
-			console.log(!/^[a-z0-9_]+$/.test(value_), !/[a-z]/g.test(value_));
-			if (!/^[a-z0-9_]+$/.test(value_)) {
-				setError(`อนุญาติ a-z, 0-9, _ เท่านั้น`);
-				// console.log('no');
-				valid = false;
-			} else if (!/[a-z]/g.test(value_)) {
-				setError(`ต้องมีตัวอักษรภาษาอังกฤษตัวพิมพ์อย่างน้อย 1 ตัว`);
-				// console.log('no');
-				valid = false;
-			} else {
-				// console.log('yes');
-				valid = true;
-			}
-		} else if (allow === 'number') {
-			if (value_ >= min && value_ <= max) {
-				valid = true;
-			} else {
-				valid = false;
-				setError(`กรุณาใส่ค่าในช่วง ${min} ถึง ${max}`);
-			}
-		}
+    if (value_.length < minLength) {
+      valid = false;
+      setError(`ความยาวต้องมากกว่า ${minLength} ตัวอักษร`);
+    } else if (value_.length > maxLength) {
+      setError(`ความยาวห้ามเกิน ${maxLength} ตัวอักษร`);
+      valid = false;
+    } else if (allow === "email") {
+      // console.log(validator.isEmail(e.target.value), e.target.value);
+      if (!validator.isEmail(value_)) {
+        setError(`รูปแบบของ email ไม่ถูกต้อง`);
+        valid = false;
+      } else {
+        valid = true;
+      }
+    } else if (allow === "username") {
+      // console.log(!/^[a-z0-9_]+$/.test(value_), !/[a-z]/g.test(value_));
+      if (!/^[a-z0-9_]+$/.test(value_)) {
+        setError(`อนุญาติ a-z, 0-9, _ เท่านั้น`);
+        // console.log('no');
+        valid = false;
+      } else if (!/[a-z]/g.test(value_)) {
+        setError(`ต้องมีตัวอักษรภาษาอังกฤษตัวพิมพ์อย่างน้อย 1 ตัว`);
+        // console.log('no');
+        valid = false;
+      } else {
+        // console.log('yes');
+        valid = true;
+      }
+    } else if (allow === "number") {
+      if (value_ >= min && value_ <= max) {
+        valid = true;
+      } else {
+        valid = false;
+        setError(`กรุณาใส่ค่าในช่วง ${min} ถึง ${max}`);
+      }
+    }
 
-		if (valid) {
-			setError();
-			e.target.error = false;
-			// console.log('clear!');
-		} else {
-			e.target.error = true;
-		}
-		onChange(e);
-	};
+    if (valid) {
+      setError();
+      e.target.error = false;
+      // console.log('clear!');
+    } else {
+      e.target.error = true;
+    }
+    onChange(e);
+  };
 
-	const onChangeHandleDropdown = (e) => {
-		// console.log(e.target.value);
-		onChange(e);
-	};
+  const onChangeHandleDropdown = (e) => {
+    // console.log(e.target.value);
+    onChange(e);
+  };
 
-	return (
-		<div className='input_container'>
-			<div className='input_title'>{children}</div>
-			<div className='input_box'>
-				{type !== 'combobox' ? (
-					<input
-						id={id}
-						className='input_input'
-						placeholder={placeholder || ''}
-						value={value}
-						onChange={handleValidation}
-						type={type || 'input'}
-						onClick={() => {
-							type === 'date' && toggleDatePicker(!openDatePicker);
-						}}
-						readonly={type === 'date' ? 'readonly' : false}
-					/>
-				) : (
-					<select
-						className='select_input'
-						onChange={onChangeHandleDropdown}
-						id={id}
-						value={value}
-					>
-						{options_.map((value, key) => {
-							return (
-								<option key={key} value={value}>
-									{value}
-								</option>
-							);
-						})}
-					</select>
-				)}
-				{type !== 'combobox' && <div className='input_ext'>{ext}</div>}
-			</div>
-			{openDatePicker && (
-				<Calendar
-					className='input_calendar'
-					maxDate={new Date()}
-					locale='th'
-					onChange={onChangeHandleCalendar}
-					value={typeof value === 'string' ? new Date(value) : value}
-				/>
-			)}
-			{error && <div className='input_error_box'>{error}</div>}
-		</div>
-	);
+  return (
+    <div className='input_container'>
+      <div className='input_title'>{children}</div>
+      <div className='input_box'>
+        {type !== "combobox" ? (
+          <input
+            id={id}
+            className='input_input'
+            placeholder={placeholder || ""}
+            value={value}
+            onChange={handleValidation}
+            type={type || "input"}
+            onClick={() => {
+              type === "date" && toggleDatePicker(!openDatePicker);
+            }}
+            readonly={type === "date" ? "readonly" : false}
+          />
+        ) : (
+          <select
+            className='select_input'
+            onChange={onChangeHandleDropdown}
+            id={id}
+            value={value}
+          >
+            {options_.map((value, key) => {
+              return (
+                <option key={key} value={value}>
+                  {value}
+                </option>
+              );
+            })}
+          </select>
+        )}
+        {type !== "combobox" && <div className='input_ext'>{ext}</div>}
+      </div>
+      {openDatePicker && (
+        <Calendar
+          className='input_calendar'
+          maxDate={new Date()}
+          locale='th'
+          onChange={onChangeHandleCalendar}
+          value={typeof value === "string" ? new Date(value) : value}
+        />
+      )}
+      {error && <div className='input_error_box'>{error}</div>}
+    </div>
+  );
 }
 
 export default Input;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ButtonLink from '../components/ButtonLink';
-import Logo from '../components/Logo';
-import '../styles/home.css';
+import React from "react";
+import ButtonLink from "../components/ButtonLink";
+import Logo from "../components/Logo";
+import "../styles/home.css";
 
 function Home() {
-	return (
-		<div className='home noselect'>
-			<div className='logo_layout'>
-				<Logo h='200px' w='200px' />
-			</div>
-			<div className='home_button_zone'>
-				<ButtonLink to='/login' height='70px'>
-					Login
-				</ButtonLink>
-				<ButtonLink to='/agreement' height='70px'>
-					Register
-				</ButtonLink>
-			</div>
-		</div>
-	);
+  return (
+    <div className='home noselect'>
+      <div className='logo_layout'>
+        <Logo h='200px' w='200px' />
+      </div>
+      <div className='home_button_zone'>
+        <ButtonLink to='/login' height='70px'>
+          Login
+        </ButtonLink>
+        <ButtonLink to='/agreement' height='70px'>
+          Register
+        </ButtonLink>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
