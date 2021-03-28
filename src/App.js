692460d018c2a016e37bf765846edc60bcd605cc
eXit-Guy <39361943+eXitHere@users.noreply.github.com>
2021-03-28 23:15:21 +0700

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import Home from "./screens/Home";
-import Agreement from "./screens/Agreement";
-import Login from "./screens/Login";
-import { Main } from "./screens/Main";
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import Home from './screens/Home';
+import Agreement from './screens/Agreement';
+import Login from './screens/Login';
+import { Main } from './screens/Main';
 // Friend zone
 import FriendsView from './screens/Friends/FriendsView';
 import { ManageFriendRequest } from './screens/Friends/ManageRequest';
 
-import { ShowProfile, EditProfile } from "./screens/Profile";
-import NotFoundPage from "./screens/NotFoundPage";
-import { isLoggedIn } from "./middleware/Cookie";
-import KnowledgeMain from "./screens/Knowledge/KnowledgeMain";
+import { ShowProfile, EditProfile } from './screens/Profile';
+import NotFoundPage from './screens/NotFoundPage';
+import KnowledgeMain from './screens/Knowledge/KnowledgeMain';
+import { isLoggedIn } from './middleware/Cookie';
 
 console.warn = console.error = () => {}; // Something bad happened 🌠
 
@@ -43,7 +43,8 @@ const App = () => {
 						path='/editprofile'
 						component={(props) => <EditProfile {...props} />}
 					/>
-					<Route component={NotFoundPage} />
+					<PrivateRoute to='/knowledge' component={KnowledgeMain} />
+					<Route path='*' component={NotFoundPage} />
 				</Switch>
 			</div>
 		</Router>
@@ -51,20 +52,20 @@ const App = () => {
 };
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const isLogged = isLoggedIn();
+	const isLogged = isLoggedIn();
 
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        isLogged ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-        )
-      }
-    />
-  );
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				isLogged ? (
+					<Component {...props} />
+				) : (
+					<Redirect to={{ pathname: '/', state: { from: props.location } }} />
+				)
+			}
+		/>
+	);
 };
 
 export default App;
