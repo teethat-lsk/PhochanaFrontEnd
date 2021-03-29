3e79b6e633a73a825d857a8074d236e418296463
soon-404 <tfpsoon@gmail.com>
2021-03-29 11:03:57 +0700

    k_v1.1

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Agreement from "./screens/Agreement";
 import Login from "./screens/Login";
 import { Main } from "./screens/Main";
 // Friend zone
-import FriendsView from './screens/Friends/FriendsView';
-import { ManageFriendRequest } from './screens/Friends/ManageRequest';
+import FriendsView from "./screens/Friends/FriendsView";
+import { ManageFriendRequest } from "./screens/Friends/ManageRequest";
 
 import { ShowProfile, EditProfile } from "./screens/Profile";
 import NotFoundPage from "./screens/NotFoundPage";
@@ -20,34 +20,35 @@ import KnowledgeMain from "./screens/Knowledge/KnowledgeMain";
 console.warn = console.error = () => {}; // Something bad happened 🌠
 
 const App = () => {
-	console.log(`User had been logged ==> ${isLoggedIn()}`);
-	return (
-		<Router>
-			<div className='App'>
-				<Switch>
-					<PrivateRoute exact path='/' component={Main} />
-					<Route path='/home' component={Home}></Route>
-					<Route path='/register'>{/* <About /> */}</Route>
-					<Route path='/agreement' component={Agreement} />
-					<PrivateRoute
-						path='/friends/:pagestate'
-						component={ManageFriendRequest}
-					/>
-					<Route path='/login' component={Login} />
-					<PrivateRoute path='/friends' component={FriendsView} />
-					<PrivateRoute
-						path='/profile/:username'
-						component={(props) => <ShowProfile {...props} />}
-					/>
-					<PrivateRoute
-						path='/editprofile'
-						component={(props) => <EditProfile {...props} />}
-					/>
-					<Route component={NotFoundPage} />
-				</Switch>
-			</div>
-		</Router>
-	);
+  console.log(`User had been logged ==> ${isLoggedIn()}`);
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+          <PrivateRoute exact path='/' component={Main} />
+          <Route path='/home' component={Home}></Route>
+          <Route path='/register'>{/* <About /> */}</Route>
+          <Route path='/agreement' component={Agreement} />
+          <PrivateRoute
+            path='/friends/:pagestate'
+            component={ManageFriendRequest}
+          />
+          <Route path='/login' component={Login} />
+          <PrivateRoute path='/friends' component={FriendsView} />
+          <PrivateRoute path='/books' component={KnowledgeMain} />
+          <PrivateRoute
+            path='/profile/:username'
+            component={(props) => <ShowProfile {...props} />}
+          />
+          <PrivateRoute
+            path='/editprofile'
+            component={(props) => <EditProfile {...props} />}
+          />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
